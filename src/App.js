daebbcfff3e4bfbd5478e3ffa954656f111dfe85
jbrock1981 <jbrock1981@gmail.com>
2018-12-27 10:16:19 -0600

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Coming Soon!</h1>
+                <h1>Are you looking for a developer?</h1>
             </div>
         );
     }
