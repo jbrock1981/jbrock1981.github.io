7bf4a67f0dc4e0c92447b6d0586b5fa2d05afc2f
jbrock1981 <jbrock1981@gmail.com>
2018-12-27 12:54:34 -0600

    landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Button } from 'reactstrap';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: black;
+    height: 100vh;
+    width: 100vw;
+    color: white;
+`
+const ButtonBox = styled.div`
+    width: 10vw;
+    display: flex;
+    justify-content: space-between;
+`
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <Container>
                 <h1>Are you looking for a developer?</h1>
-            </div>
+                <ButtonBox>
+                    <Button color="success">Yes</Button>
+                    <Button color="danger">No</Button>
+                </ButtonBox>
+            </Container>
         );
     }
 }
