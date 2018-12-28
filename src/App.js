7ed383b07af05875871a5c5fbdf52d63e20cb1df
jbrock1981 <jbrock1981@gmail.com>
2018-12-28 14:00:05 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import { Button } from 'reactstrap';
+
+function hideNoButton() {
+    return `
+        background-color: black;
+        border: 1px solid black;
+        cursor: none;
+        color: black;
+        ${NoButton.textContent = "Yes!"}
+    `
+    
+}
 
 const Container = styled.div`
     display: flex;
@@ -11,21 +23,61 @@ const Container = styled.div`
     height: 100vh;
     width: 100vw;
     color: white;
+
+`
+const QuestionBox = styled.div`
+    width: 70vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 `
 const ButtonBox = styled.div`
-    width: 10vw;
+    width: 150px;
     display: flex;
     justify-content: space-between;
+    margin-top: 30px;
 `
+const YesButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 40px;
+    background-color: charcoal;
+    border-radius: 15%;
+    border: 1px solid gray;
+    cursor: pointer;
+    :hover {
+        background-color: gray;
+    }    
+`
+let NoButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width 50px;
+    height: 40px;
+    background-color: charcoal;
+    border-radius: 15%;
+    border: 1px solid gray;
+    cursor: pointer;
+    &:hover {
+        ${hideNoButton};
+    }
+`
+
 
 class App extends Component {
     render() {
         return (
             <Container>
-                <h1>Are you looking for a developer?</h1>
+                <QuestionBox>
+                    <h1>Are you looking for a developer?</h1>
+                </QuestionBox>
                 <ButtonBox>
-                    <Button color="success">Yes</Button>
-                    <Button color="danger">No</Button>
+                    <YesButton>Yes</YesButton>
+                    <NoButton>No</NoButton>
                 </ButtonBox>
             </Container>
         );
